843bc83e0b1e0db280998a8ec36ee0c1729bd53b
Jlow <hed-fjnieves@smu.edu.ph>
2024-10-10 20:38:14 +0900

    Facial Recognition System  - Live Camera UI done.

---
+++ FISTML_CS_v0.0/Form2.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace FISTML_CS_v0._0
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbLive = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lbLive
+            // 
+            lbLive.AutoSize = true;
+            lbLive.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbLive.Location = new Point(48, 33);
+            lbLive.Name = "lbLive";
+            lbLive.Size = new Size(113, 21);
+            lbLive.TabIndex = 0;
+            lbLive.Text = "LIVE CAMERA";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveBorder;
+            pictureBox1.Location = new Point(48, 65);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(695, 356);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(204, 445);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 48);
+            button1.TabIndex = 2;
+            button1.Text = "CHANGE CAMERA";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(413, 445);
+            button2.Name = "button2";
+            button2.Size = new Size(140, 48);
+            button2.TabIndex = 3;
+            button2.Text = "EXIT TO MENU";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(793, 522);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(lbLive);
+            Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Name = "Form2";
+            Text = "Facial Recognition System  - Live Camera";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbLive;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
